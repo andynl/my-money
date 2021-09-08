875502e53d7db9445564f80ebeba33ef50517b9e
Andy Natalino <andy.natalino@gmail.com>
2021-09-08 18:21:43 +0700

    add income and outcome fake api'

--- controller/transaction_controller.go
+++ controller/transaction_controller.go
@@ -34,6 +34,19 @@ var transactions = []transaction{
 	{ID: "6", Type: "Outcome", CategoryID: "6", Name: "Beli Ayam Geprek", Amount: 10000, CreatedAt: time.Now()},
 }
 
+
+var incomes = []transaction{
+	{ID: "1", Type: "Income", CategoryID: "1",Name: "Gaji Bulan September 2021", Amount: 45000000, CreatedAt: time.Now()},
+	{ID: "2", Type: "Income", CategoryID: "2", Name: "Jual Tanah Warisan", Amount: 100000000,  CreatedAt: time.Now()},
+	{ID: "3", Type: "Income", CategoryID: "3", Name: "Arisan", Amount: 15000000, CreatedAt: time.Now()},
+}
+
+var outcomes = []transaction{
+	{ID: "4", Type: "Outcome", CategoryID: "4", Name: "Iphone 12 Pro", Amount: 13000000, CreatedAt: time.Now()},
+	{ID: "5", Type: "Outcome", CategoryID: "5", Name: "Baju Bagus", Amount: 1000000, CreatedAt: time.Now()},
+	{ID: "6", Type: "Outcome", CategoryID: "6", Name: "Beli Ayam Geprek", Amount: 10000, CreatedAt: time.Now()},
+}
+
 var summaries = summary{BalanceLastMonth: 34000000, BalanceThisMonth: 35000000}
 
 type TransactionController struct{}
@@ -44,6 +57,8 @@ func NewTransactionController() TransactionController {
 
 func (controller *TransactionController) Route(app *fiber.App) {
 	app.Get("/api/transactions", controller.Get)
+	app.Get("/api/transactions/income", controller.Income)
+	app.Get("/api/transactions/outcome", controller.Outcome)
 	app.Get("/api/v2/transactions", controller.Get2)
 }
 
@@ -53,10 +68,11 @@ func (controller *TransactionController) Get(c *fiber.Ctx) error {
 	// type: income, expense
 	// sort by: highest, lowest
 
-	//msg := fmt.Sprintf("limit %s - type %s", c.Query("limit"), c.Query("type"))
+	//msg := fmt.Sprintf("limit %s - type %s", c.QueryParser("limit"), c.Query("type"))
 	//return c.SendString(msg)
+	//limit := c.Query("limit")
 
-	//trxData := TransactionData{summaries, transactions}
+ 	//trxData := TransactionData{summaries, transactions}
 	return c.JSON(transactions)
 }
 
@@ -67,4 +83,12 @@ func (controller *TransactionController) Get2(c *fiber.Ctx) error {
 		Status: "OK",
 		Data: trxData,
 	})
+}
+
+func (controller *TransactionController) Income(c *fiber.Ctx) error {
+	return c.JSON(incomes)
+}
+
+func (controller *TransactionController) Outcome(c *fiber.Ctx) error {
+	return c.JSON(outcomes)
 }
